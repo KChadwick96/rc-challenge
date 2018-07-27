97dbbe3196c93fe57692bd7599e8702ec71d5afd
Kieran <kieran.chadwick@outlook.com>
2018-07-27 20:17:31 +0100

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="search-container">
+          <h2>Where are you going?</h2>
+
+          <div className="location-finder">
+            <label for="location-input">Pick-up Location</label>
+            <input id="location-input" placeholder="city, airport, station, region, district..."  aria-label="" />
+          </div>
+        </div>
       </div>
     );
   }
