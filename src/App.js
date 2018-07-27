9eabb1d02cf337837afe98607d47baf354d1a415
Kieran <kieran.chadwick@outlook.com>
2018-07-27 20:44:46 +0100

    aria input attributes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
 
           <div className="location-finder">
             <label for="location-input">Pick-up Location</label>
-            <input id="location-input" placeholder="city, airport, station, region, district..."  aria-label="" />
+            <input id="location-input" placeholder="city, airport, station, region, district..."
+              aria-autocomplete="list"
+              aria-haspopup="true" />
           </div>
         </div>
       </div>
